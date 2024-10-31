fadb5ad9564067d99011b6b2c0a44897ad28868e
zn <zn@pamocloud.com>
2024-10-31 17:22:43 +0800

    first commit

---
+++ Objects/main.d
@@ -0,0 +1,5 @@
+.\objects\main.o: User\main.c
+.\objects\main.o: .\Start\stm32f10x.h
+.\objects\main.o: .\Start\core_cm3.h
+.\objects\main.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\main.o: .\Start\system_stm32f10x.h
---
+++ Objects/system_stm32f10x.d
@@ -0,0 +1,5 @@
+.\objects\system_stm32f10x.o: Start\system_stm32f10x.c
+.\objects\system_stm32f10x.o: Start\stm32f10x.h
+.\objects\system_stm32f10x.o: Start\core_cm3.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\system_stm32f10x.o: Start\system_stm32f10x.h
